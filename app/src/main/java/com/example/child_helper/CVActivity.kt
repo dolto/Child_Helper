efc18a0088d7a991063e5da7c135438eca06e786
qkrwlrkd <dkfb22@gmail.com>
2023-10-12 18:08:25 +0900

    page make

---
+++ app/src/main/java/com/example/child_helper/CVActivity.kt
@@ -0,0 +1,11 @@
+package com.example.child_helper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CVActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cvactivity)
+    }
+}
