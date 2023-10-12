d84e3aba26cd61d89ebcb4bcab674494095c2492
qkrwlrkd <dkfb22@gmail.com>
2023-10-12 18:44:20 +0900

    page make2

--- app/src/main/java/com/example/child_helper/FPActivity.kt
+++ app/src/main/java/com/example/child_helper/FPActivity.kt
@@ -1,11 +1,20 @@
 package com.example.child_helper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class FPActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fpactivity)
+        setTitle("지문 인식");
+
+        var fingerBtn: View = findViewById(R.id.fingerBtn)
+        fingerBtn.setOnClickListener {
+            var intent = Intent(applicationContext, CVActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/child_helper/MainActivity.kt
+++ app/src/main/java/com/example/child_helper/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.child_helper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.child_viewer)
+        setContentView(R.layout.main_page)
+        setTitle("메인 페이지");
+
+        val Btn_Help: View = findViewById(R.id.Btn_Help)
+        val Btn_Register : View = findViewById(R.id.Btn_Register)
+        Btn_Help.setOnClickListener{
+            val  intent = Intent(applicationContext, FPActivity::class.java)
+            startActivity(intent)
+
+
+        }
+        Btn_Register.setOnClickListener {
+            val intent = Intent(applicationContext, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
